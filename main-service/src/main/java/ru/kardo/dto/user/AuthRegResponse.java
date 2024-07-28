16c763f18779b11e05dcb5a9dc98ef89aa9359f9
Ovabor <ovabor1991@gmail.com>
2024-07-28 18:45:12 +0300

    changed response from auth to json

---
+++ main-service/src/main/java/ru/kardo/dto/user/AuthRegResponse.java
@@ -0,0 +1,15 @@
+package ru.kardo.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthRegResponse {
+
+    private String token;
+}
