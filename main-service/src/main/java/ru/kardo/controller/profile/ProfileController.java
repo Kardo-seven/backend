9dc92b01b517f970018f9424f78bcc6f2f9692fb
Ovabor <ovabor1991@gmail.com>
2024-07-28 16:16:59 +0300

    add cors for controllers

--- main-service/src/main/java/ru/kardo/controller/profile/ProfileController.java
+++ main-service/src/main/java/ru/kardo/controller/profile/ProfileController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequestMapping("/profile")
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin
 public class ProfileController {
 
     private final ProfileService profileService;
