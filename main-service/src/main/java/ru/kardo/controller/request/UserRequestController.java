7429f62c0828f14ccb7677542610d86e39a74ced
Ovabor <ovabor1991@gmail.com>
2024-07-31 01:46:35 +0300

    some refact

--- main-service/src/main/java/ru/kardo/controller/request/UserRequestController.java
+++ main-service/src/main/java/ru/kardo/controller/request/UserRequestController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.kardo.dto.request.RequestPreviewDtoResponse;
