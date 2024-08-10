db9a5b97ffa596df9703eb046b248539278a2e83
Ovabor <ovabor1991@gmail.com>
2024-08-10 10:08:15 +0300

    rework event scheme, update data sql, update images for events

--- main-service/src/main/java/ru/kardo/model/Event.java
+++ main-service/src/main/java/ru/kardo/model/Event.java
@@ -25,43 +25,43 @@ public class Event {
     @Column(name = "event_id")
     private Long id;
 
-    @Column(name = "event_date")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private LocalDate eventDate;
-
-    @Column(name = "start_time")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime startTime;
-
-    @Column(name = "end_time")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime endTime;
-
-    @Column(name = "description")
-    private String description;
+//    @Column(name = "event_date")
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+//    private LocalDate eventDate;
+//
+//    @Column(name = "start_time")
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+//    private LocalDateTime startTime;
+//
+//    @Column(name = "end_time")
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+//    private LocalDateTime endTime;
+//
+//    @Column(name = "description")
+//    private String description;
 
     @Column(name = "event_type")
     @Enumerated(EnumType.STRING)
     private EventType eventType;
 
-    @Column(name = "location")
-    private String location;
+//    @Column(name = "location")
+//    private String location;
 
     @Column(name = "title")
     private String title;
 
-    @Column(name = "is_grand_final_event")
-    private Boolean isGrandFinalEvent;
+//    @Column(name = "is_grand_final_event")
+//    private Boolean isGrandFinalEvent;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "event_id", unique = true)
     private EventImage eventImage;
 
-    @ElementCollection(fetch = FetchType.LAZY)
-    @CollectionTable(name = "event_directions" , joinColumns = @JoinColumn(name = "event_id"))
-    @Column(name = "direction")
-    @AttributeOverrides({
-            @AttributeOverride(name = "direction", column = @Column(name = "direction"))
-    })
-    private Set<Direction> directionSet;
+//    @ElementCollection(fetch = FetchType.LAZY)
+//    @CollectionTable(name = "event_directions" , joinColumns = @JoinColumn(name = "event_id"))
+//    @Column(name = "direction")
+//    @AttributeOverrides({
+//            @AttributeOverride(name = "direction", column = @Column(name = "direction"))
+//    })
+//    private Set<Direction> directionSet;
 }
